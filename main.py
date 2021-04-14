acff913acaaac20bc05027938c4c5be4712132c6
Sebastian Hamza <>
2021-04-14 16:40:58 +0000

    Added API connections

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import discord
 import os
+import requests
+import json
 
 client = discord.Client()
 
@@ -7,13 +9,33 @@ client = discord.Client()
 async def on_ready():
   print('We have logged in as {0.user}'.format(client))
 
+def get_joke():
+  response = requests.get("https://v2.jokeapi.dev/joke/Any")
+  json_data = json.loads(response.text)
+  if(json_data['type']=='twopart'):
+    joke = json_data['setup'] + json_data['delivery']
+  else:
+    joke = json_data['joke']
+  return(joke)
+
+def get_dog():
+  response = requests.get("https://dog.ceo/api/breeds/image/random")
+  json_data = json.loads(response.text)
+  image = json_data['message']
+  return image
+
 
 @client.event
 async def on_message(message):
   if message.author == client.user:
     return
   
-  if message.content.startswith('$georgiana'):
-    await message.channel.send('Georgianaa_fr spune: hai noroc', tts=True)
+  if message.content.startswith('>joke'):
+    joke = get_joke()
+    await message.channel.send(joke, tts='true')
+  
+  if message.content.startswith('>dog'):
+    image = get_dog()
+    await message.channel.send(image)
 
 client.run(os.getenv('TOKEN'))
