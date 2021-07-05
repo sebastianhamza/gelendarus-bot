ca725ffe1798e7cfe2dbc30f3d7e40664b458367
Sebastian Hamza <sebby_99@yahoo.com>
2021-07-05 07:53:11 +0000

    Changed the way that commands work. Added new commands: dictionary, info.

--- main.py
+++ main.py
@@ -3,12 +3,42 @@ import os
 import requests
 import json
 import random
+from discord.ext import commands
+from keep_alive import keep_alive
 
-client = discord.Client()
+#client = discord.Client()
 
-@client.event
+bot = commands.Bot(command_prefix= '>')
+
+
+
+@bot.event
 async def on_ready():
-  print('We have logged in as {0.user}'.format(client))
+  await bot.change_presence(activity=discord.Game(name="Use >info for commands"))
+  print('We have logged in as {0.user}'.format(bot))
+
+@bot.command()
+async def info(ctx):
+    embedVar = discord.Embed(title="Commands", description="", color=0x00ff00)
+    embedVar.add_field(name="dictionary", value="Provides the definition of the argument", inline=False)
+    embedVar.add_field(name="meme", value="Posts a meme", inline=False)
+    await ctx.send(embed=embedVar)
+
+@bot.command()    
+async def dictionary(ctx, arg):
+    await ctx.send(get_dictionary(arg))
+
+@bot.command()    
+async def meme(ctx):
+    await ctx.send(get_meme())
+
+
+
+def get_dictionary(word):
+  response = requests.get("https://api.dictionaryapi.dev/api/v2/entries/en_US/{}".format(word))
+  json_data = json.loads(response.text)
+  meaning = json_data[0]['meanings'][0]['definitions'][0]['definition']
+  return(meaning)
 
 def get_joke():
   response = requests.get("https://v2.jokeapi.dev/joke/Any")
@@ -26,6 +56,13 @@ def get_meme():
   image = arr[len(arr)-1]
   return image
 
+def get_meme_dark():
+  response = requests.get("https://meme-api.herokuapp.com/gimme/torridmemes")
+  json_data = json.loads(response.text)
+  arr = json_data['preview']
+  image = arr[len(arr)-1]
+  return image
+
 def get_dog():
   response = requests.get("https://dog.ceo/api/breeds/image/random")
   json_data = json.loads(response.text)
@@ -36,27 +73,36 @@ def roll_dice():
   number = random.randint(1, 6)
   return number
 
-@client.event
-async def on_message(message):
-  if message.author == client.user:
-    return
+# @client.event
+# async def on_message(message):
+#   if message.author == client.user:
+#     return
   
-  if message.content.startswith('>joke'):
-    joke = get_joke()
-    await message.channel.send(joke, tts='true')
+#   if message.content.startswith('>joke'):
+#     joke = get_joke()
+#     await message.channel.send(joke, tts='true')
   
-  if message.content.startswith('>dog'):
-    image = get_dog()
-    await message.channel.send(image)
+#   if message.content.startswith('>dog'):
+#     image = get_dog()
+#     await message.channel.send(image)
 
-  if message.content.startswith('>meme'):
-    image = get_meme()
-    await message.channel.send(image)
+#   if message.content.startswith('>meme'):
+#     image = get_meme()
+#     await message.channel.send(image)
 
-  if message.content.startswith('>dice'):
-    dice = roll_dice()
-    await message.channel.send(dice)
+#   if message.content.startswith('>dark'):
+#     image = get_meme_dark()
+#     await message.channel.send(image)
+
+#   if message.content.startswith('>dice'):
+#     dice = roll_dice()
+#     await message.channel.send(dice)
+
+#   if message.content.startswith('>help'):
+#     await message.channel.send(">meme, >dark, >dice")
 
 
 
-client.run(os.getenv('TOKEN'))
+keep_alive()
+
+bot.run(os.getenv('TOKEN'))
